00fcce9ebf6d6f45603c4c217ed6331266a3bf2b
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2022-09-14 23:04:22 +0530

    git remote add origin https://github.com/Tanmaydeep-Singh/corazon-para-ti.git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const body = document.querySelector("body");
+
+  function createHeartOne() {
+    const heart = document.createElement("div");
+    heart.className = "heartMoveOne";
+    heart.innerHTML = "❤️";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+    body.appendChild(heart);
+  }
+
+  function createHeartTwo() {
+    const heart = document.createElement("div");
+    heart.className = "heartMoveTwo";
+    heart.innerHTML = "❤️";
+    heart.style.top = Math.random() * 100 + "vh";
+    heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+    body.appendChild(heart);
+  }
+
+  function createHeartThree() {
+    const heart = document.createElement("div");
+    heart.className = "heartMoveThree";
+    heart.innerHTML = "❤️";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+    body.appendChild(heart);
+  }
+
+  
+  document.addEventListener('keydown', (event) => {
+    var keyValue = event.key;
+
+    if( keyValue.toLowerCase() === 'a')
+    {
+      createHeartTwo();
+    }
+
+    if( keyValue.toLowerCase() ==='s')
+    {
+      createHeartOne();
+    }
+    
+    if( keyValue.toLowerCase() === 'd')
+    {
+      createHeartThree();
+    }
+  })
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container">
+        <button onClick={createHeartOne}> Down</button>
+      </div>
+
+      <div id="container">
+        <button onClick={createHeartTwo}> Right</button>
+      </div>
+      <div id="container">
+        <button onClick={createHeartThree}> Diagonal</button>
+      </div>
     </div>
   );
 }
